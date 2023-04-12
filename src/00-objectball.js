0939a0a12c476619dc929c49ec3905369286a74c
Evan <eroberts1998@icloud.com>
2023-04-12 13:15:04 -0400

    complete first function

--- src/00-objectball.js
+++ src/00-objectball.js
@@ -115,3 +115,44 @@ function gameObject() {
     };
   }
 
+  function homeTeamName() {
+    let object = gameObject();
+    return object["home"]["teamName"];
+  }
+
+  console.log(homeTeamName());
+
+  function awayTeamName() {
+    let object = gameObject();
+    return object["away"]["teamName"];
+  }
+
+  console.log(awayTeamName());
+
+
+// Build a function, numPointsScored that takes in an argument of a player's name and returns the number of points scored for that player
+
+function numPointsScored(playerName) {
+  let object = gameObject();
+  for (let team in object) {
+    let players = object[team].players;
+    if (playerName in players) {
+      return players[playerName].points;
+    }
+  }
+  return null;
+}
+
+console.log(numPointsScored("Alan Anderson")); // Returns 22
+
+// Build a function, shoeSize, that takes in an argument of a player's name and returns the shoe size for that player
+
+// Build a function, teamColors, that takes in an argument of the team name and returns an array of that teams colors.
+
+// Build a function, teamNames, that operates on the game object to return an array of the team names.
+
+// Build a function, playerNumbers, that takes in an argument of a team name and returns an array of the jersey numbers for that team.
+
+// Build a function, playerStats, that takes in an argument of a player's name and returns an object of that player's stats
+
+// Build a function, bigShoeRebounds, that will return the number of rebounds associated with the player that has the largest shoe size. Break this one down into steps:
